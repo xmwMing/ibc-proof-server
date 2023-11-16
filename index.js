165d148a748c7e050e191ad47761809d783d959a
Ming <xumingwei_0@163.com>
2023-11-16 15:51:34 +0800

    优化入口代码,引入redis

--- index.js
+++ index.js
@@ -1,77 +1,69 @@
-require('dotenv').config(); 
+require('dotenv').config();
 //create websocket server
 const WebSocketServer = require('ws');
+const Redis = require("ioredis");
 const websocketPort = process.env.PORT;
+const redisHost = process.env.REDIS_HOST;
+const redisPort = process.env.REDIS_PORT;
 
 //import websocket message handlers
-const { handleHeavyProof, handleLightProof, handleGetBlockActions, handleGetDbStatus } = require("./handlers");
-
-const { closeClientStreams } = require("./firehoseFunctions");
+const {handleHeavyProof} = require("./handlers");
 
 //main process handling websocket connections
-const main = async ()=> {
-  //initiate websocket server
-  const wss = new WebSocketServer.Server({ port: websocketPort });
+const main = async () => {
+    //initiate websocket server
+    console.log("redis host = " + redisHost)
+    const wss = new WebSocketServer.Server({port: websocketPort});
+    console.log(redisHost)
+    const redis = new Redis(redisHost, redisPort);
+    //on new connection handler
+    wss.on("connection", (ws, req) => {
+        ws.id = req.headers['sec-websocket-key'];
+        console.log("new ws connected");
 
-  //on new connection handler
-  wss.on("connection", (ws, req) => {
-    ws.id = req.headers['sec-websocket-key'];
-    console.log("new ws connected");
+        ws.on("message", async (message) => {
+            let msgObj;
+            try {
+                msgObj = JSON.parse(message.toString())
+            } catch (ex) {
+                return ws.send(JSON.stringify({type: "error", error: "Message needs to be a stringified object"}));
+            }
+            await handleHeavyProof(msgObj.blockNum, redis,ws);
+        });
 
-    ws.on("message", async (message) => {
-      let supportedTypes = ['lightProof', 'heavyProof', 'getBlockActions', 'getDbStatus'];
-      //ensure message is in the right format
-      let msgObj;
-      try{ msgObj = JSON.parse(message.toString())}
-      catch(ex){ return ws.send(JSON.stringify({ type:"error", error: "Message needs to be a stringified object" })); }
-      //handle unsupported type
-      if (!supportedTypes.includes(msgObj.type)) return ws.send(JSON.stringify({ type:"error", error: "Message needs to include request types:" + supportedTypes.join(',') }));
-      //handle getDbStatus
-      if (msgObj.type == "getDbStatus") return handleGetDbStatus(ws);
-      
-      if(isNaN(msgObj.block_to_prove)) return ws.send(JSON.stringify({ type:"error", error: "Must supply block_to_prove number"  }));
-      console.log("#####################################################")
-      console.log("message",message.toString());
-      //handle request according to type
-      if (msgObj.type == "lightProof") {
-        if(isNaN(msgObj.last_proven_block)) return ws.send(JSON.stringify({ type:"error", error: "Must supply last_proven_block number"  }));
-        handleLightProof(msgObj, ws);
-      }
-      else if (msgObj.type == "heavyProof") handleHeavyProof(msgObj, ws);
-      else if (msgObj.type == "getBlockActions") handleGetBlockActions(msgObj, ws);
+        //find and close existing firehose streams if socket client disconnects
+        ws.on("close", () => {
+            console.log("the ws has disconnected");
+            // closeClientStreams(ws.id);
+        });
+
+        ws.onerror = function (e) {
+            console.log("Error",e)
+        }
     });
-
-    //find and close existing firehose streams if socket client disconnects
-    ws.on("close", () => {
-      console.log("the ws has disconnected");
-      closeClientStreams(ws.id);
-    });
-
-    ws.onerror = function (e) { console.log("Error".e) }
-  });
-  console.log("Listening on", websocketPort)
+    console.log("Listening on", websocketPort)
 }
 
 var signals = {
-  'SIGHUP': 1,
-  'SIGINT': 2,
-  'SIGTERM': 15
+    'SIGHUP': 1,
+    'SIGINT': 2,
+    'SIGTERM': 15
 };
 Object.keys(signals).forEach((signal) => {
-  process.on(signal, () => {
-    console.log(`process received a ${signal} signal`);
-    let value = signals[signal];
-    process.exit(128 + value);
-  });
+    process.on(signal, () => {
+        console.log(`process received a ${signal} signal`);
+        let value = signals[signal];
+        process.exit(128 + value);
+    });
 });
 
 main().catch(error => {
-  console.log("unhandled error main", error)
-  process.exit(1);
+    console.log("unhandled error main", error)
+    process.exit(1);
 });
 
 process.on("unhandledRejection", function (reason, p) {
-  let message = reason ? reason.stack : reason;
-  console.error(`Possibly Unhandled Rejection at: ${message}`);
-  process.exit(1);
+    let message = reason ? reason.stack : reason;
+    console.error(`Possibly Unhandled Rejection at: ${message}`);
+    process.exit(1);
 });
